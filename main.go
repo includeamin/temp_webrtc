c15477feff11209727d5949491ae3c097e414466
aminjamal <aminjamal10@gmail.com>
2020-09-12 03:20:26 +0430

    add basic files

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	//"github.com/gin-gonic/autotls"
-	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+
 	"github.com/pion/webrtc/v3"
 	"github.com/povilasv/prommod"
 	"github.com/prometheus/client_golang/prometheus"
@@ -36,7 +37,7 @@ func Init() {
 
 func main() {
 	Init()
-	r := gin.Default()
+	//r := gin.Default()
 	if err := prometheus.Register(prommod.NewCollector("sfu_ws")); err != nil {
 		panic(err)
 	}
@@ -44,28 +45,28 @@ func main() {
 	port := flag.String("p", "8443", "https port")
 	flag.Parse()
 
-	//http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics", promhttp.Handler())
 
 	// Websocket handle func
-	r.GET("/ws", func(c *gin.Context) {
-		room(c.Writer, c.Request)
-	})
-	//http.HandleFunc("/ws", room)
+	//r.GET("/ws", func(c *gin.Context) {
+	//	room(c.Writer, c.Request)
+	//})
+	http.HandleFunc("/ws", room)
 
 	// Html handle func
-	r.GET("/", func(context *gin.Context) {
-		web(context.Writer, context.Request)
-	})
-	//http.HandleFunc("/", web)
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+	///	r.GET("/", func(context *gin.Context) {
+	//		web(context.Writer, context.Request)
+	//	})
+	http.HandleFunc("/", web)
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.String(200, "pong")
+	//})
 
 	// Support https, so we can test by lan
 	fmt.Println("Web listening :" + *port)
-	//panic(http.ListenAndServeTLS(":"+*port, "cert.pem", "key.pem", nil))
+	panic(http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil))
 	//panic(http.ListenAndServe("0.0.0.0:8080", nil))
 	//log.Fatal(autotls.Run(r, "includeamin.kelidiha.com"))
-	r.RunTLS(":8443", "./cert.pem", "./key.pem")
+	//r.RunTLS(":8443", "./cert.pem", "./key.pem")
 	//r.Run("0.0.0.0:8080")
 }
